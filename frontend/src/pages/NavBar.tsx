02c9b39eed85ef9897068693ae71dfbe371fd687
Cheryl Tan <cheryltan134@gmail.com>
2024-02-11 18:10:27 +0800

    finished base frontend pages

--- frontend/src/pages/NavBar.tsx
+++ frontend/src/pages/NavBar.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 const NavigationBar: React.FC = () => {
   return (
     <Navbar bg="white" expand="lg" className="justify-content-between">
       <Container>
-        <Navbar.Brand href="#home">Packy</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Packy</Navbar.Brand>
         <Nav>
-          <Nav.Link href="#create">
+          <Nav.Link as={Link} to="/create">
             <Button variant="primary">Create</Button>
           </Nav.Link>
           <Nav.Link href="#settings">
             <i className="bi bi-gear large-icon"></i>
           </Nav.Link>
-          <Nav.Link href="#profile">
+          <Nav.Link as={Link} to="/user">
             <i className="bi bi-person-circle large-icon"></i>
           </Nav.Link>
         </Nav>
---
+++ frontend/src/pages/PackageCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PackageCard: React.FC = () => {
+
+  return (
+    <div className="container mt-4">
+      <h2>Edit Package</h2>
+    </div>
+  );
+};
+
+export default PackageCard;
